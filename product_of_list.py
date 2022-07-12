626a1880d243487ca4f80af3ad01cf8c16ac8a94
Fawaz Azeez <49353217+aziz300@users.noreply.github.com>
2022-07-12 22:34:26 +0100

    Add files via upload
    1) The file ( product_of_list) contains program that multiplies all the items in a list

---
+++ product_of_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def productof(items):
+    product = 1
+    for i in items:
+        product *= i
+    return product
+
+
+# In[8]:
+
+
+productof([2,3,6])
+
+
+# In[ ]:
+
+
+
+
