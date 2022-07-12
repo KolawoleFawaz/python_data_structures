53b74d70071ce6fba6231d950db4364e9d6b6a1a
Fawaz Azeez <49353217+aziz300@users.noreply.github.com>
2022-07-12 22:39:16 +0100

    Add files via upload

---
+++ squared.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[36]:
+
+
+def squared(n):
+    squares= {i:i*i for i in range(1,n+1)}
+    
+    return squares
+
+
+# In[37]:
+
+
+squared(8)
+
+
+# In[ ]:
+
+
+
+
